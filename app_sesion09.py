bf0348991829b0bc544a656b521f23adfa001219
xaramillo <39895039+xaramillo@users.noreply.github.com>
2025-06-25 22:44:06 +0000

    sesion 09 cierre

--- app_sesion09.py
+++ app_sesion09.py
@@ -24,7 +24,29 @@ with open('/workspaces/ml-bootcamp-labs/models/model_op.pkl', 'rb') as file:
 def prediccion_datos(campos,valores):
     diccionario = dict(zip(campos,valores))
     data = pd.DataFrame(diccionario,index=[0])
-    return model.predict(data)
+    # 3. Validación de datos
+    # Well_ID {Los valores válidos para well ID son los pozos: 1 al 20}
+    pozos_validos = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+    # Hay una forma más elegante que es con range(1,21,1)
+    if data['Well_ID'][0] not in pozos_validos:
+        st.error(f"Pozo {data.Well_ID.values} no es un valor válido (1-20)")
+        return None
+    # Mantenimiento
+    mantenimiento_validos = [0,1]
+    if data['Maintenance_Required'][0] not in mantenimiento_validos:
+        st.error(f"Valor en Maintenance_Required no es un valor válido (0,1)")
+        return None
+    else:
+        return model.predict(data)
+
+def remapeo_resultados(resultado):
+    res_diccionario = {
+        0:'Blockage',
+        1:'Leakage',
+        2:'Normal',
+        3:'Pump Failure'
+    }
+    return res_diccionario[int(resultado)]
 
 # 2. Carga de datos
 
@@ -40,10 +62,14 @@ st.markdown("Introduce los datos de operación para predecir:")
 # Ciclo: Se encarga de desplegar un formulario en la aplicación
 for c in campos:
     v = st.text_input(f"{c} (numérico)")
-    valores.append(v)
+    valores.append(float(v))
 
 
 # 4. Predicción
 if st.button("Predecir!"):
+    # 3. Validación
     resultado = prediccion_datos(campos,valores)
-    st.success(f"Status operativo: {resultado}")
+    if resultado is None:
+        st.error("No se pudo completar la predicción")
+    else:    
+        st.success(f"Status operativo: {remapeo_resultados(resultado)}")
